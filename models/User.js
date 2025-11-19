2eef1f9aefdab10c844844a0209aabf9dee0da43
samer123e-design <samer123e@gmail.com>
2025-11-19 23:09:42 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  role: { 
+    type: String, 
+    enum: ['user', 'moderator', 'admin'], 
+    default: 'user' 
+  },
+  banned: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('User', UserSchema);
